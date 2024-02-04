63c5aa2bff17bcc6b5733726f04f29f49b8592a3
Paulo Nadales <p.nadales.j@gmail.com>
2024-02-03 23:09:55 -0300

    Se agregan funcionalidad con JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
+$("a").click(function (event) {
 
+    if (this.hash !== "") {
 
+        event.preventDefault();
 
-$(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > $("#seccion").offset().top) {
-            $('.negro').addClass("azul");
-        } else {
-            $(".negro").removeClass("azul");
-        }
-    });
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 250); // primero probar con 0 y aumente de 1000 en 1000 para ver diferencia
+    }
+});
+var tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+);
+var tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+$("#BotonContacto").click(function (e) {
+    e.preventDefault();
+    alert("¡Mensaje enviado correcamente!");
 });
